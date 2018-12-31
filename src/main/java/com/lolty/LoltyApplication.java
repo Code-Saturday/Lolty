f2b1762bccbd7a3c0c79eae0922664c1325c2c56
brianmituka <mitukab@outlook.com>
2018-12-31 13:07:53 +0300

    Initial push to github :rocket:

---
+++ src/main/java/com/lolty/LoltyApplication.java
@@ -0,0 +1,14 @@
+package com.lolty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoltyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoltyApplication.class, args);
+	}
+
+}
+
